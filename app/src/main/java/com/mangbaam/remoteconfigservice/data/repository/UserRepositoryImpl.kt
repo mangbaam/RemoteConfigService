c5e43a4aee8e0eeec2e6ae819358ebf86fe8f374
mangbaam <pmb0836@gmail.com>
2024-08-24 23:14:12 +0900

    UserRepositoryImpl.getUser 개선

--- app/src/main/java/com/mangbaam/remoteconfigservice/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/mangbaam/remoteconfigservice/data/repository/UserRepositoryImpl.kt
@@ -5,27 +5,21 @@ import com.mangbaam.remoteconfigservice.domain.model.User
 import com.mangbaam.remoteconfigservice.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 internal class UserRepositoryImpl @Inject constructor(
     private val userDataSource: UserDataSource,
 ) : UserRepository {
-    override fun getUser(): Flow<User> = flow {
-        combine(
-            flowOf(userDataSource.getNickname()),
-            flowOf(userDataSource.getAge()),
-            flowOf(userDataSource.getIsMarried()),
-            flowOf(userDataSource.getSkills()),
-        ) { nickname, age, isMarried, skills ->
-            listOf(0, 1).random().let {
-                if (it == 1) error("invalid nickname")
-            }
-            User(nickname, age, isMarried, skills)
+    override fun getUser(): Flow<User> = combine(
+        flow { emit(userDataSource.getNickname()) },
+        flow { emit(userDataSource.getAge()) },
+        flow { emit(userDataSource.getIsMarried()) },
+        flow { emit(userDataSource.getSkills()) },
+    ) { nickname, age, isMarried, skills ->
+        listOf(0, 1).random().let {
+            if (it == 1) error("invalid nickname")
         }
-            .first()
-            .let { emit(it) }
+        User(nickname, age, isMarried, skills)
     }
 }
